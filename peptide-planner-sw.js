11f99102e92ee414ea14cad51e5afa04ad1e23d4
jesykadelacruz <84807985+jesykadelacruz@users.noreply.github.com>
2026-09-09 14:25:39 +0800

    Sync service worker to v2026.0909.04

--- peptide-planner-sw.js
+++ peptide-planner-sw.js
@@ -1,4 +1,4 @@
-const PEPTIDE_PLANNER_SW_VERSION='v2026.0909.03';
+const PEPTIDE_PLANNER_SW_VERSION='v2026.0909.04';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
