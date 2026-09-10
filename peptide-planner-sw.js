3718bcab938af4a768eb5a0b3001946c7ac7458e
jesykadelacruz <84807985+jesykadelacruz@users.noreply.github.com>
2026-09-10 06:37:22 +0000

    Peptide_Planner_v20261009.08

--- peptide-planner-sw.js
+++ peptide-planner-sw.js
@@ -1,4 +1,4 @@
-const PEPTIDE_PLANNER_SW_VERSION='Peptide_Planner_v20261009.07';
+const PEPTIDE_PLANNER_SW_VERSION='Peptide_Planner_v20261009.08';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
